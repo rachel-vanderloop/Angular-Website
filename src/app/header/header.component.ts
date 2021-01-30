158aacbc7325d75ff580920c3dfc4b269d60d735
Rachel Vanderloop <rvanderl@uwo.ca>
2021-01-29 19:15:06 -0500

    adding button transitions to the transparent header transition

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,9 +19,19 @@ window.onscroll = function() {makeTransparent()};
 function makeTransparent() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     var element = document.getElementById("transparent-nav")!;
+    var buttonHome = document.getElementById("button-home")!;
+    var buttonResume = document.getElementById("button-resume")!;
+
     element.classList.replace("bg-transparent", "bg-info");
+    buttonHome.classList.replace("btn-outline-dark", "btn-dark");
+    buttonResume.classList.replace("btn-outline-dark", "btn-dark");
+
   } else {
     var element = document.getElementById("transparent-nav")!;
+    var buttonHome = document.getElementById("button-home")!;
+    var buttonResume = document.getElementById("button-resume")!;
     element.classList.replace("bg-info", "bg-transparent");
+    buttonHome.classList.replace("btn-dark", "btn-outline-dark");
+    buttonResume.classList.replace("btn-dark", "btn-outline-dark");
   }
 }
